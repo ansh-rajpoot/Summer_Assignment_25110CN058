7cd83c01f59e0d039e4cd7ffde14001a047edd41
Ansh Rajpoot <anshrajpoot428@gmail.com>
2026-06-08 19:55:30 +0530

    Day-8 uploaded

---
+++ Day_8/ques32.py
@@ -0,0 +1,15 @@
+# Q32 Write a program to Print repeated-number
+# pattern.
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+
+n= int(input('Enter the number of rows: '))
+for i in range(1,n+1):
+     
+    for j in range(1,i+1):
+        print(i,end='')
+    print()
